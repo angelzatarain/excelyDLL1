5707d1ec53aa377000fb315c518ee6918d77e906
dai <dai@itam.mx>
2019-11-12 12:48:47 -0600

    primeros ejercicios DLL y excel

---
+++ PruebaDLL2/PruebaDLL2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//Los siguientes 3 usings los necesitamos y se deben poner despues de poner la referencia
+using Microsoft.Office.Interop;
+using e = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+
+namespace PruebaDLL2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Varaibles Globales
+        e.Application excelApp;
+        e.Workbook libro;
+        e.Worksheet hoja;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String fileTest = @"c:\ana\pruebaDll2.xlsx";
+            if (File.Exists(fileTest))
+                File.Delete(fileTest);
+            excelApp = new e.Application();
+            libro = excelApp.Workbooks.Add();
+            hoja = (e.Worksheet)libro.Worksheets.get_Item(1);
+            hoja.Cells[1, 1] = textBox.Text;
+
+            libro.SaveAs(fileTest);
+            libro.Close();
+            excelApp.Quit();
+            MessageBox.Show("El libro fue creado");
+        }
+
+    }
+}
---
+++ pruebaDLL3/pruebaDLL3/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaDLL3
+{
+    public class Class1
+    {
+        //Por interoperabilidad ya no haremos staticos sino una clase
+        private int num1 = 5;
+        private int num2 = 3;
+
+        public void setNum1(int numero) {
+            num1 = numero;
+        }
+
+        public void setNum2(int numero) {
+            num2 = numero;
+        }
+
+        public int multiplica() {
+            return num1 * num2;
+        }
+    }
+}
